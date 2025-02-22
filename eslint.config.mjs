3bc25455419b43059aeef7261097d8a92e177d2f
Harvey Yemm <hjyemm@icloud.com>
2025-02-22 22:11:09 +0000

    changed linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,15 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default {
+  root: true,
+  extends: ['next', 'next/core-web-vitals'],
+  rules: {
+    'no-unused-vars': 'off',   // Disable unused variable warnings
+    '@typescript-eslint/no-unused-vars': 'off',  // Disable unused variable warnings for TypeScript
+    '@typescript-eslint/no-explicit-any': 'off', // Disable explicit 'any' type rule
+    'react-hooks/exhaustive-deps': 'off', // Disable missing dependency warnings for hooks
+    // Add more rules as necessary
+  },
+};
+
