2bc59d2f18cb149534af8527f8d4e9899dc8c63a
Harvey Yemm <hjyemm@icloud.com>
2025-02-22 22:14:16 +0000

    changed linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,18 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
-export default {
-  root: true,
-  extends: ['next', 'next/core-web-vitals'],
-  rules: {
-    'no-unused-vars': 'off',   // Disable unused variable warnings
-    '@typescript-eslint/no-unused-vars': 'off',  // Disable unused variable warnings for TypeScript
-    '@typescript-eslint/no-explicit-any': 'off', // Disable explicit 'any' type rule
-    'react-hooks/exhaustive-deps': 'off', // Disable missing dependency warnings for hooks
-    // Add more rules as necessary
-  },
-};
-
+export default eslintConfig;
